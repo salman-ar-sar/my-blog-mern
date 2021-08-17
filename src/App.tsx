9beaf9501bb6fabcaae07a033258a2a9d93d485d
Salman A <salman.a@hatio.in>
2021-08-17 11:40:26 +0530

    Add routing for 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Article from "./pages/Article";
 import ArticleListPage from "./pages/ArticleListPage";
 import NavBar from "./components/NavBar";
+import PageNotFound from "./pages/PageNotFound";
 
 function App(): JSX.Element {
   return (
@@ -12,10 +13,13 @@ function App(): JSX.Element {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={About} exact />
-          <Route path="/article/:name" component={Article} exact />
-          <Route path="/articles-list" component={ArticleListPage} exact />
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={About} />
+            <Route path="/article/:name" component={Article} />
+            <Route path="/articles-list" component={ArticleListPage} />
+            <Route component={PageNotFound} />
+          </Switch>
         </div>
       </div>
     </Router>
