df62919202652d2ed9e90d5c2a6d5e9aedd5be9e
Salman A <salman.a@hatio.in>
2021-08-17 11:12:01 +0530

    Add individual article rendering

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,17 +1,29 @@
 import { match } from "react-router-dom";
+import articles, { Article } from "../data/ArticleContent";
 
 interface Params {
   name: string;
 }
 
-const Article = ({ match }: { match: match<Params> }): JSX.Element => {
+const ArticlePage = ({ match }: { match: match<Params> }): JSX.Element => {
   const name = match.params.name;
+
+  const article: Article | undefined = articles.find(
+    (article) => article.name === name
+  );
+
+  if (!article) {
+    return <h1>No article found!</h1>;
+  }
 
   return (
     <>
-      <h1>{name} Article!</h1>
+      <h1>{article?.title}</h1>
+      {article?.content.map((para, key) => (
+        <p key={key}>{para}</p>
+      ))}
     </>
   );
 };
 
-export default Article;
+export default ArticlePage;
